7cd03216caecbf7488c3159b336ce2588a3ad10b
andrey <andrey@moshbear.net>
2025-07-07 03:45:28 -0400

    fix deadlock arising from wrong dispatcher

--- server/src/main/kotlin/Application.kt
+++ server/src/main/kotlin/Application.kt
@@ -2,11 +2,13 @@ package com.moshy.drugcalc.server
 
 import com.moshy.drugcalc.server.http.createKtorServer
 import com.moshy.drugcalc.server.util.AppConfig
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
     val config = AppConfig.config(args)
-    runBlocking {
+    // we need IO dispatcher to prevent deadlock arising from request -> cache miss -> fetch -> db io
+    runBlocking(Dispatchers.IO) {
         createKtorServer(config)
             .apply { start(wait = true) }
     }
