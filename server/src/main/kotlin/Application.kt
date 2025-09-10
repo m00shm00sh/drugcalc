00df4540cd6953e817f2f4cf6d4005ef2343eb8c
andrey <andrey@moshbear.net>
2025-09-10 04:04:22 -0400

    server: on-launch logging fixups

--- server/src/main/kotlin/Application.kt
+++ server/src/main/kotlin/Application.kt
@@ -6,7 +6,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
+    // we might have logging setup in config so to avoid logging chicken and egg, just print to stdout
+    val cwd = System.getProperty("user.dir")
+    println("workdir: $cwd")
     val config = AppConfig.config(args)
+    config.http.bind.forEach {
+        println("listening on: $it")
+    }
+
     // we need IO dispatcher to prevent deadlock arising from request -> cache miss -> fetch -> db io
     runBlocking(Dispatchers.IO) {
         createKtorServer(config)
