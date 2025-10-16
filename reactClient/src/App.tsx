1148a2cad0057a49c8947fcede24473ed2462a7b
andrey <andrey@moshbear.net>
2025-10-16 01:19:45 -0400

    use tabs instead of react-router

--- reactClient/src/App.tsx
+++ reactClient/src/App.tsx
@@ -1,42 +1,74 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import './App.css'
+import 'react-tabs/style/react-tabs.css'
 import { Login, Logout } from './components/Auth'
 import { Calc } from './components/Calc'
 import { BlendsEditor } from './components/item-editor/BlendsEditor'
 import { CompoundsEditor } from './components/item-editor/CompoundsEditor'
 import { ConfigEditor } from './components/item-editor/ConfigEditor'
 import { FrequenciesEditor } from './components/item-editor/FrequenciesEditor'
-import { NavBar } from './components/NavBar'
 import { useLocalToken } from './hooks/useLocalData'
+import { Flex } from './widgets/RowCol'
+import { Centered } from './widgets/Centered'
 
 const App = () => {
     const loginToken = useLocalToken()[0]
     const isLoggedIn = !!loginToken
+    const loginProps = { loginToken, isLoggedIn, }
     return (
-        <BrowserRouter>
-            <NavBar isLoggedIn={isLoggedIn} />
-            <Routes>
-                <Route path="/" element={<Calc />} />
-                <Route path="edit">
-                    <Route path="config" element={<ConfigEditor />} />
-                    <Route
-                        path="compounds"
-                        element={
-                            <CompoundsEditor isLoggedIn={isLoggedIn} loginToken={loginToken} />
+        <>
+            <title>Drugcalc</title>
+            <Flex dir="col">
+                <Tabs>
+                    <Centered>
+                        <TabList>
+                            <Tab>
+                                Calc
+                            </Tab>
+                            <Tab>
+                                Editor
+                            </Tab>
+                            <Tab>
+                                Auth
+                            </Tab>
+                        </TabList>
+                    </Centered>
+                    <TabPanel>
+                        <Calc />
+                    </TabPanel>
+                    <TabPanel>
+                        <Tabs>
+                            <Centered>
+                                <TabList>
+                                    <Tab>Compounds</Tab>
+                                    <Tab>Blends</Tab>
+                                    <Tab>Frequencies</Tab>
+                                    <Tab>Config</Tab>
+                                </TabList>
+                            </Centered>
+                            <TabPanel>
+                                <CompoundsEditor {...loginProps} />
+                            </TabPanel>
+                            <TabPanel>
+                                <BlendsEditor {...loginProps} />
+                            </TabPanel>
+                            <TabPanel>
+                                <FrequenciesEditor {...loginProps} />
+                            </TabPanel>
+                            <TabPanel>
+                                <ConfigEditor />
+                            </TabPanel>
+                        </Tabs>
+                    </TabPanel>
+                    <TabPanel>
+                        {isLoggedIn
+                            ? <Logout />
+                            : <Login />
                         }
-                    />
-                    <Route path="blends" element={<BlendsEditor isLoggedIn={isLoggedIn} />} />
-                    <Route
-                        path="frequencies"
-                        element={<FrequenciesEditor isLoggedIn={isLoggedIn} />}
-                    />
-                </Route>
-                <Route path="auth">
-                    <Route path="login" element={<Login />} />
-                    <Route path="logout" element={<Logout />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+                    </TabPanel>
+                </Tabs>
+            </Flex>
+        </>
     )
 }
 
