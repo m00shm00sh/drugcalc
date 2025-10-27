e8c141d03cf730798f29ad02442588da536cd5b9
andrey <andrey@moshbear.net>
2025-10-27 03:31:47 -0400

    reshaper: add readonly to input arrays and objects

--- reactClient/src/util/reshaper.ts
+++ reactClient/src/util/reshaper.ts
@@ -1,5 +1,12 @@
+type TryReadonly<V> = V extends number | string | boolean
+    ? V
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    : V extends Array<infer _>
+        ? ReadonlyArray<V>
+        : Readonly<V>
+
 export function fieldsToMap<Row, V>(
-    rows: Row[],
+    rows: ReadonlyArray<Row>,
     toKey: (_: Row) => string,
     toValue: (_: Row) => V,
 ): Record<string, V> {
@@ -7,10 +14,11 @@ export function fieldsToMap<Row, V>(
     return Object.fromEntries(kvs)
 }
 
+
 export function mapToFields<V, Row>(
-    map: Record<string, V>,
+    map: Record<string, TryReadonly<V>>,
     fromKey: (key: string) => Partial<Row>,
-    fromValue: (value: V, currentState: Partial<Row>) => Row,
+    fromValue: (value: TryReadonly<V>, currentState: Partial<Row>) => Row,
 ): Row[] {
     return Object.entries(map).map(([k, v]) => {
         const o: Partial<Row> = fromKey(k)
