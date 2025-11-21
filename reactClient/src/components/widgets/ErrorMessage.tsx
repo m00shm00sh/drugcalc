22cdfb5f708b758939c3324d4a345644eb8b5888
andrey <andrey@moshbear.net>
2025-11-21 06:24:52 -0500

    ErrorMessage: use Centered

--- reactClient/src/components/widgets/ErrorMessage.tsx
+++ reactClient/src/components/widgets/ErrorMessage.tsx
@@ -1,6 +1,8 @@
+import { Centered } from "./Centered"
+
 export const ErrorMessage = ({ text }: { text?: string }) => (
-    <div className={'text-red-600 text-sm align-middle text-center'}>{text}</div>
+    <Centered className='text-red-600 text-sm'>{text}</Centered>
 )
 export const BigErrorMessage = ({ text }: { text?: string }) => (
-    <div className={'text-red-600 text-xl align-middle text-center'}>{text}</div>
+    <Centered className='text-red-600 text-xl'>{text}</Centered>
 )
